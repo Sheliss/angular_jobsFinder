432d4ed5146533cccee6ebc806015743a6755525
Nikita Kobylkin <sheliss.kai@gmail.com>
2022-11-01 14:43:57 +0200

    jobs service works

--- src/app/globalComponents/jobs.service.ts
+++ src/app/globalComponents/jobs.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Job } from '../Interfaces/JobInfo';
+import { Job } from '../Interfaces/jobInfo';
 
 @Injectable({
   providedIn: 'root'
---
+++ src/app/jobs-board/board/board.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { JobsService } from '../../globalComponents/jobs.service';
+import { Job } from 'src/app/Interfaces/jobInfo';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent implements OnInit {
+  jobs: Job[] = [];
+
+  constructor(private jobsService: JobsService) { }
+
+  ngOnInit(): void {
+    this.jobsService.getJobs().subscribe((jobs: Job[]) => (this.jobs = jobs));
+  }
+
+}
