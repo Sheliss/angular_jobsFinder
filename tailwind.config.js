3ed8d5484bedd78eb6ec3ec18f8d3339960c2f30
Nikita Kobylkin <sheliss.kai@gmail.com>
2022-11-03 13:16:08 +0200

    board styling done, need mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'mont': ['Montserrat', 'sans-serif']
+    },
+    colors: {
+      'white': '#fff',
+      'main-blue': '#3A4562',
+      'card-gray': '#878D9D'
+    },
+    boxShadow: {
+      'card-shadow': '2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12)'
+    }
   },
   plugins: [],
 }
