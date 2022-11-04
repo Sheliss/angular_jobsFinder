dc3cdab011723ea35f53376a93f461ad2ca0e9a1
Nikita Kobylkin <sheliss.kai@gmail.com>
2022-11-04 17:44:23 +0200

    job page almost done, needs map and mobile styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,20 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      'mont': ['Montserrat', 'sans-serif']
+      'mont': ['Montserrat', 'sans-serif'],
+      'roboto': ['Roboto', 'sans-serif']
     },
     colors: {
       'white': '#fff',
       'main-blue': '#3A4562',
-      'card-gray': '#878D9D'
+      'card-gray': '#878D9D',
+      'sub-blue' : '#38415d',
+      'card-b-text': '#55699E',
+      'card-b-bg': '#a1b1db',
+      'card-b-border': '#55699e',
+      'card-y-text': '#988B49',
+      'card-y-bg': '#ffcf00',
+      'card-y-border': '#FFCF00'
     },
     boxShadow: {
       'card-shadow': '2px 1px 7px rgba(0, 0, 0, 0.08), 0px 2px 1px -1px rgba(0, 0, 0, 0.04), 0px 1px 3px rgba(0, 0, 0, 0.12)'
